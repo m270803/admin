92da9a5c47273efa2290cc33c7de7b6aa18d9ab8
Shad0w8 <75994175+Shad0w8@users.noreply.github.com>
2024-05-12 23:08:36 +0530

    routes added

--- routes/users.js
+++ routes/users.js
@@ -87,4 +87,22 @@ router.post('/login',[
     }
   });
 
+router.post('/Work',uploadmiddleware.single('file'), async (req,res) =>{
+  const {originalname,path,} = req.file;
+  const part = originalname.split('.');
+  const ext = part[part.length - 1];
+  const newPath = path+'.'+ext;
+  fs.renameSync(path, newPath);
+      const {fullname,lastname,title,intro}=req.body;
+      const postDoc = await Hero.create({
+          //require the data in form and assign it here to store in the database
+          profilePicUrl:newPath,
+          fullName: fullname/*name from form */,
+          lastName: lastname/*name from form */ ,
+          titles: title/*title from form */,
+          introduction: intro/*paragraph from form */,
+      });
+      res.json(postDoc);
+});
+
 module.exports=router
