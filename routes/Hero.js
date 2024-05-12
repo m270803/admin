20f1753dd3b09e301d795f938576742f078aa43b
Shad0w8 <75994175+Shad0w8@users.noreply.github.com>
2024-05-12 23:08:56 +0530

    routes added

--- routes/Hero.js
+++ routes/Hero.js
@@ -1,25 +1,49 @@
 const express=require('express');
 const router=express.Router();
 const Hero=require('../models/Hero');
+const multer=require('multer');
 const uploadmiddleware = multer({dest:'uploads/'});
 const fs = require('fs');
 
-router.post('/Hero',uploadmiddleware.single('file'), async (req,res) =>{
-    const {originalname,path,} = req.file;
-    const part = originalname.split('.');
-    const ext = part[part.length - 1];
-    const newPath = path+'.'+ext;
-    fs.renameSync(path, newPath);
-        const {fullname,lastname,title,intro}=req.body;
-        const postDoc = await Hero.create({
-            //require the data in form and assign it here to store in the database
-            profilePicUrl:newPath,
-            fullName: fullname/*name from form */,
-            lastName: lastname/*name from form */ ,
-            titles: title/*title from form */,
-            introduction: intro/*paragraph from form */,
-        });
+router.post('/', uploadmiddleware.single('file'), async (req, res) => {
+    try {
+        if (!req.file) {
+            const { fullname, lastname, title, intro } = req.body;
+
+            const postDoc = await Hero.create({
+                // profilePicUrl: newPath,
+                fullName: fullname,
+                lastName: lastname,
+                titles: title,
+                introduction: intro
+            });
+
         res.json(postDoc);
+            return res.status(400).json({ error: 'No file uploaded' });
+        }
+
+        const { originalname, path } = req.file;
+        const part = originalname.split('.');
+        const ext = part[part.length - 1];
+        const newPath = path + '.' + ext;
+
+        await fs.rename(path, newPath);
+
+        const { fullname, lastname, title, intro } = req.body;
+
+        const postDoc = await Hero.create({
+            profilePicUrl: newPath,
+            fullName: fullname,
+            lastName: lastname,
+            titles: title,
+            introduction: intro
+        });
+
+        res.json(postDoc);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
 
 module.exports=router;
--- routes/users.js
+++ routes/users.js
@@ -86,23 +86,6 @@ router.post('/login',[
       res.status(500).send("Some error occurred");
     }
   });
-
-router.post('/Work',uploadmiddleware.single('file'), async (req,res) =>{
-  const {originalname,path,} = req.file;
-  const part = originalname.split('.');
-  const ext = part[part.length - 1];
-  const newPath = path+'.'+ext;
-  fs.renameSync(path, newPath);
-      const {fullname,lastname,title,intro}=req.body;
-      const postDoc = await Hero.create({
-          //require the data in form and assign it here to store in the database
-          profilePicUrl:newPath,
-          fullName: fullname/*name from form */,
-          lastName: lastname/*name from form */ ,
-          titles: title/*title from form */,
-          introduction: intro/*paragraph from form */,
-      });
-      res.json(postDoc);
-});
+  
 
 module.exports=router
